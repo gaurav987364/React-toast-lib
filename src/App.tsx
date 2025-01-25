9c345693b79e79599baffc78faf704411d545d22
gaurav <145903987+gaurav987364@users.noreply.github.com>
2025-01-25 21:02:23 +0530

    ...

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
+import Toast from "./components/Toast"
+
 const App = () => {
   return (
-    <div className=' w-full h-screen bg-slate-950 text-neutral-100 flex items-center justify-center'>App</div>
+    <div className=' w-full h-screen bg-slate-950 text-neutral-100 flex items-center justify-center'>
+      <Toast/>
+    </div>
   )
 }
 
---
+++ src/components/Toast.tsx
@@ -0,0 +1,19 @@
+const Toast = () => {
+  return (
+    <div className=" w-[450px] h-[600px] border rounded-sm">
+        <div className=" p-2 flex flex-col justify-center items-center">
+            <h1 className=" text-center text-3xl font-mono">
+                React Toast🍞
+            </h1>
+            <div>
+                image
+            </div>
+            <div>
+                content
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Toast;
